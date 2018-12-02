647535e0336c58908950030775176bf58d427fb1
nguyhai <tenth11@gmail.com>
2018-12-02 01:43:21 -0600

    Add project files.

---
+++ RestAPIAuthDemo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestAPIAuthDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RestAPIAuthDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestAPIAuthDemo.WebApiApplication" Language="C#" %>
